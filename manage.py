e62b4b8ade4880341e301b4a502d556b46e04569
Hoang Tran <revend1234@gmail.com>
2020-06-07 22:42:12 -0700

    python club

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PythonClub.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PythonClub_proj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
