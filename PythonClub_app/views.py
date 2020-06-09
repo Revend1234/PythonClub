9aa82442039130993fa4e2af8ddf32a256f0eecf
Hoang Tran <revend1234@gmail.com>
2020-06-09 15:33:35 -0700

    assignment 5 6

--- PythonClub_app/views.py
+++ PythonClub_app/views.py
@@ -4,6 +4,17 @@ from .models import Meeting, MeetingMinutes, Resource, Event
 def index (request):
     return render(request, 'club/index.html')
 
-def getresource(request):
-    resource_list=ProductType.objects.all()
-    return render(request,'pythonclub_app/resource_list',{'resource_list' : resource_list})
+def getresources(request):
+    resources_list=Resource.objects.all()
+    return render(request,'templates/resources.html',{'resources_list' : resources_list})
+
+def getmeetings(request):
+    meetings_list=Meeting.objects.all()
+    return render(request, 'templates/meetings.html', {'meetings_list': meetings_list})
+
+ def meetingdetails(request, id):
+    meet=get_object_or_404(Meeting, pk=id)
+    context={
+        'meet' : meet,
+    }
+    return render(request, 'techapp/meetdetails.html', context=context)
