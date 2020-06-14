e665a70e1d2fdf5aa7bbb05cada35fdbebbcf4f2
Hoang Tran <revend1234@gmail.com>
2020-06-14 16:32:58 -0700

    updated directory

--- PythonClub_app/views.py
+++ PythonClub_app/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from .models import Meeting, MeetingMinutes, Resource, Event
 # Create your views here.
 def index (request):
-    return render(request, 'club/index.html')
+    return render(request, 'templates/club/index.html')
 
 def getresources(request):
     resources_list=Resource.objects.all()
-    return render(request,'templates/resources.html',{'resources_list' : resources_list})
+    return render(request,'templates/club/resources.html',{'resources_list' : resources_list})
 
 def getmeetings(request):
     meetings_list=Meeting.objects.all()
-    return render(request, 'templates/meetings.html', {'meetings_list': meetings_list})
+    return render(request, 'templates/club/meetings.html', {'meetings_list': meetings_list})
 
  def meetingdetails(request, id):
     meet=get_object_or_404(Meeting, pk=id)
     context={
         'meet' : meet,
     }
-    return render(request, 'techapp/meetdetails.html', context=context)
+    return render(request, 'templates/club/meetdetails.html', context=context)
